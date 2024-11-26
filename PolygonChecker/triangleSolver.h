89e0b975028f8bbff9073991a54942fd8e7f6dae
Akekic <akekic1433@conestogac.on.ca>
2024-11-26 16:51:06 -0500

    fixed analyze4Points(), and added compareDoubles()

--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// The approximate value of PI
 #define M_PI 3.14159265358979323846
 
 double* getTriangleSides(double* triangleSides);
@@ -7,5 +8,5 @@ double* getTriangleSides(double* triangleSides);
 char* analyzeTriangle(double side1, double side2, double side3);
 
 // Takes three side lengths of a triangle,
-// and returns the angle opposite to the first side agruement
+// and returns the angle opposite to the FIRST side agruement
 double getAngle(double a, double b, double c);
